82335bf2530971d761450099ac85c99be8540877
phuong2106 <phuong.nguyen@cel-consulting.com>
2019-10-14 17:38:53 +0700

    [server] Update testing database.

--- server/scripts/restore-test-dbs.sh
+++ server/scripts/restore-test-dbs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mongorestore "tests/business/random_test" --host 127.0.0.1 --port 27017 --drop --db random_test
+mongorestore "tests/business/ITV" --host 127.0.0.1 --port 27017 --drop --db ITV
