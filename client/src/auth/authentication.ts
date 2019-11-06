2a84cf047bd89198541d8f0992d0f158c52123ca
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2019-11-06 11:52:51 +0700

    Correct server host

--- client/src/auth/authentication.ts
+++ client/src/auth/authentication.ts
@@ -3,7 +3,7 @@ import { store } from '../store/store'
 import { ActionType } from '../actions/ActionType'
 import { AuthAction } from '../actions/AuthAction'
 
-export const ServerHost = "itv-server"
+export const ServerHost = "localhost"
 export const ServerPort = "5001"
 
 export const IsAuthenticated = async (username: string, password: string) => {
@@ -12,7 +12,9 @@ export const IsAuthenticated = async (username: string, password: string) => {
         Password: password
     }
     let bRes = false
-    await axios.post("http://" + ServerHost + ":" + ServerPort +  "/apis/internal/user/login", user).then((response) => {
+    let url = "http://" + ServerHost + ":" + ServerPort +  "/apis/internal/user/login"
+    console.log("IsAuthenticated:", url)
+    await axios.post(url, user).then((response) => {
         let {access_token, refresh_token} = response.data
         let action: AuthAction = {
             type: ActionType.UPDATE_TOKEN_PAIR,
