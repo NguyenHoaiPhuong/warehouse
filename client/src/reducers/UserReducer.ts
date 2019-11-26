567e67a76f4b4911328b1a7564b88879cc56a0aa
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2019-11-26 11:07:36 +0700

    [Client] Remove log msg.

--- client/src/reducers/UserReducer.ts
+++ client/src/reducers/UserReducer.ts
@@ -5,16 +5,10 @@ import { UserAction } from "../actions/UserAction";
 export function UserReducers(prevState: UserState = InitialUserState, action: UserAction): UserState {
     switch (action.type) {
         case ActionType.LOGIN_REQUEST:
-            console.log(prevState)
-            console.log(action.payload)
             return prevState
         case ActionType.LOGIN_SUCCESS:
-            console.log(prevState)
-            console.log(action.payload)
             return prevState
         case ActionType.LOGIN_FAIL:
-            console.log(prevState)
-            console.log(action.payload)
             return prevState
         default:
             return prevState
