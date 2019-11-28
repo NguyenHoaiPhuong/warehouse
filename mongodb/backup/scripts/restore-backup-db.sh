1f6d16ce79f76dc8a115a2a00ca7b81abf09c49d
Nguyen Hoai Phuong <phuong.nguyen@cel-consulting.com>
2019-11-28 18:35:20 +0700

    [Backup] Don't use host mongo command, but docker mongo cmd.

--- mongodb/backup/scripts/restore-backup-db.sh
+++ mongodb/backup/scripts/restore-backup-db.sh
@@ -16,4 +16,4 @@ echo "${YELLOW}Default username${RESTORE}: ${GREEN}$DEFAULT_USER${RESTORE}"
 echo "${YELLOW}Default password${RESTORE}: ${GREEN}$DEFAULT_PASS${RESTORE}"
 echo "${YELLOW}Default database${RESTORE}: ${GREEN}$DEFAULT_DB${RESTORE}"
 
-mongorestore $MONGO_PATH/$DEFAULT_DB --host ${DEFAULT_HOST} --port ${DEFAULT_PORT} --drop --db ${DEFAULT_DB} --username ${DEFAULT_USER} --password ${DEFAULT_PASS} --authenticationDatabase admin
+docker exec -t itv-mongo mongorestore $MONGO_PATH/$DEFAULT_DB --host ${DEFAULT_HOST} --port ${DEFAULT_PORT} --drop --db ${DEFAULT_DB} --username ${DEFAULT_USER} --password ${DEFAULT_PASS} --authenticationDatabase admin
--- mongodb/backup/scripts/save-backup-db.sh
+++ mongodb/backup/scripts/save-backup-db.sh
@@ -16,4 +16,4 @@ echo "${YELLOW}Default username${RESTORE}: ${GREEN}$DEFAULT_USER${RESTORE}"
 echo "${YELLOW}Default password${RESTORE}: ${GREEN}$DEFAULT_PASS${RESTORE}"
 echo "${YELLOW}Default database${RESTORE}: ${GREEN}$DEFAULT_DB${RESTORE}"
 
-mongodump --out $MONGO_PATH/ --host ${DEFAULT_HOST} --port ${DEFAULT_PORT} --db ${DEFAULT_DB} --username ${DEFAULT_USER} --password ${DEFAULT_PASS} --authenticationDatabase admin
+docker exec -t itv-mongo mongodump --out $MONGO_PATH/ --host ${DEFAULT_HOST} --port ${DEFAULT_PORT} --db ${DEFAULT_DB} --username ${DEFAULT_USER} --password ${DEFAULT_PASS} --authenticationDatabase admin
